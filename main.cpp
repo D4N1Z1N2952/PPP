25439fcc474a6cde78b523aef7a7dc095c3cdb11
Daniel Carvalho <danielgpcarvalho20121212@gmail.com>
2025-12-25 20:30:52 -0300

    Try this on 2.5.1 (page 39)

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 #include "PPPheaders.h"
 
 int main() {
-    cout << "Enter amn double: ";
-    int n = 0; // double n = 0;
-    cin >> n;
-    cout << "n == " << n
-         << "\n plus 1 == "<< n + 1
-         << "\nn three times == " << n*3
-         << "\nn twice == " << n*2
-         << "\nn squared == " << n*n
-         << "\nn half == " << n/2
-         << "\nn square root == " << sqrt(n)
-         << "\nn module 2 == " << n % 2
-         << '\n';
+    string previous;
+    string current;
+    int repeated = 0;
+    while (cin>>current) {
+        if (previous==current) {
+            cout << "Repeated word: " << current << '\n';
+            repeated++;
+        }
+        previous = current;
+    }
+    /* First try me:
+    * input:  The cat cat jumped
+    * output: Repeated word: cat
+    *         repeated = 1
+    */
+    /* Second try me:
+    * input: She she laughed "he he he!" because what he did did not looked too too good good
+    * output: Repeated word: did
+    *         Repeated word: too
+    *         Repeated word: good
+    *         repeated = 3
+    */
+
 }
