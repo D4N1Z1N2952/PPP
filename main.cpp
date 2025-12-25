e095a30080b59e8386c54e5fd81e51fe053146dd
Daniel Carvalho <danielgpcarvalho20121212@gmail.com>
2025-12-25 10:16:14 -0300

    Finished exercise 1.2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "PPPheaders.h"
 
 int main() {
-    std::cout << "Hello, Programming!\n";
-    std::cout << "Here we go!\n";
+    std::cout << "To get to the bathroom on the 1st floor you need to:" << '\n';
+    std::cout << "\t1 - Get upstairs\n";
+    std::cout << "\t\t1.1 - Get up\n\t\t1.2 - Go towards the stair\n\t\t1.3 - Go up the stair with one foot at a time until you get to the end of the stair\n";
+    std::cout << "\t2 - Go to the bathroom\n";
+    std::cout << "\t\t2.1 - Go forward\n\t\t2.2 - Turn to your right\n\t\t2.3 - Open the bathroom door\n\t\t2.4 - Enter the bathroom" << '\n';
+    std::cout << "Congratulations!!! You got to the bathroom\n";
 }
