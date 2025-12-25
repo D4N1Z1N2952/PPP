fb5f1027def29ace80295162b8e3806f5ccd92eb
Daniel Carvalho <danielgpcarvalho20121212@gmail.com>
2025-12-25 19:55:02 -0300

    Try this on 2.4 (page 36)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
-
 #include "PPPheaders.h"
 
 int main() {
-    cout << "Enter you name and your age: ";
-    string name;
-    int age;
-    cin >> name >> age;
-    cout << "Your name is: " << name << " and your age in months is: " << age * 12 << '\n';
+    cout << "Enter amn double: ";
+    int n = 0; // double n = 0;
+    cin >> n;
+    cout << "n == " << n
+         << "\n plus 1 == "<< n + 1
+         << "\nn three times == " << n*3
+         << "\nn twice == " << n*2
+         << "\nn squared == " << n*n
+         << "\nn half == " << n/2
+         << "\nn square root == " << sqrt(n)
+         << "\nn module 2 == " << n % 2
+         << '\n';
 }
