bab87a709d05d74a95b16436525662a94c7b6ef7
Daniel Carvalho <danielgpcarvalho20121212@gmail.com>
2025-12-26 10:09:08 -0300

    Try this on 2.6 (page 41) fixed

--- main.cpp
+++ main.cpp
@@ -1,22 +1,6 @@
 #include "PPPheaders.h"
 
-int Main() {
-    STRING s = "Goodbye,  cruel world! ";
-    cOut << S << '\n';
-    /*
-    * Users/gnti/Everything/Programming/C++/PPP/main.cpp:4:5: error: unknown type name 'STRING'
-    * 4 |     STRING s = "Goodbye,  cruel world! ";
-    *   |     ^
-    * /Users/gnti/Everything/Programming/C++/PPP/main.cpp:5:5: error: use of undeclared identifier 'cOut'; did you mean 'cout'?
-    *   5 |     cOut << S << '\n';
-    *     |     ^~~~
-    *     |     cout
-    * /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/iostream:57:42: note: 'cout' declared here
-    *  57 | extern _LIBCPP_EXPORTED_FROM_ABI ostream cout;
-    *     |                                          ^
-    * /Users/gnti/Everything/Programming/C++/PPP/main.cpp:5:13: error: use of undeclared identifier 'S'
-    *   5 |     cOut << S << '\n';
-    *     |             ^
-    * 3 errors generated.
-    */
+int main() {
+    string s = "Goodbye, cruel world! ";
+    cout << s << '\n';
 }
