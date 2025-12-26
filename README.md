50a8e6c96acdd5684f092bc3a5cd6be8cebcc6c1
Daniel Carvalho <danielgpcarvalho20121212@gmail.com>
2025-12-26 20:18:52 -0300

    Finished practice 2.1 (page 47)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PPP (Programming principles and practice using C++)
 
-This repository is disposable for holding the answers to the exercises and "Try this" sections from the Bjarne Stroustrup's book Programming Principles and practice using C++ (third edition).
+This repository is disposable for holding the answers to the exercises, practice and "Try this" sections from the Bjarne Stroustrup's book Programming Principles and practice using C++ (third edition).
 
 All answers are in the main.cpp file, after each exercise a commit is done, so for finding answers for exercises go to the commit list and search for the specific exercise. **Note that the commit order is the same order the exercises are shown in the book.**  
 
--- main.cpp
+++ main.cpp
@@ -1,32 +1,8 @@
 #include "PPPheaders.h"
 
 int main() {
-    double d = 0;
-    while (cin>>d) {
-        int i = d;
-        char c = i;
-        cout << "d==" << d
-             << " i==" << i
-             << " c==" << c
-             << " char(" << c << ")" << endl;
-    }
+    cout << "Enter the name of the person you want to send the letter: ";
+    string first_name;
+    cin >> first_name;
+    cout << "Dear " << first_name << ",\n";
 }
-
-// 32
-// d==32 i==32 c==  char( )
-// 33
-// d==33 i==33 c==! char(!)
-// 10000000
-// d==1e+07 i==10000000 c==� char(�)
-// 100000000000000000000000000000000000000000000
-// d==1e+44 i==2147483647 c==� char(�)
-// -55.67
-// d==-55.67 i==-55 c==� char(�)
-// 2
-// d==2 i==2 c== char()
-// 5
-// d==5 i==5 c== char()
-// 10
-// d==10 i==10 c==
-//  char(
-// )
