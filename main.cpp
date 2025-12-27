4a9d36da3e4682f65ddb8e5b78e252026d29d624
Daniel Carvalho <danielgpcarvalho20121212@gmail.com>
2025-12-27 16:39:30 -0300

    Finished exercise 2.8 (page 49)

--- main.cpp
+++ main.cpp
@@ -1,22 +1,12 @@
 #include "PPPheaders.h"
 
 int main() {
-    string s1, s2, s3;
-    std::cout << "Enter three values: ";
-    std::cin >> s1 >> s2 >> s3;
-    std::cout << s1 << ", " << s2 << " and " << s3 << '\n';
-
-    if (s1 >= s2 && s2 >= s3) {        // s3 s2 s1
-        std::cout << s3 << " " << s2 << " " << s1;
-    } else if (s3 >= s2 && s2 >= s1) { // s1 s2 s3
-        std::cout << s1 << " " << s2 << " " << s3;
-    } else if (s3 >= s1 && s1 >= s2) { // s2 s1 s3
-        std::cout << s2 << " " << s1 << " " << s3;
-    } else if (s1 >= s3 && s3 >= s2) { // s2 s3 s1
-        std::cout << s2 << " " << s3 << " " << s1;
-    } else if (s2 >= s3 && s3 >= s1) { // s1 s3 s2
-        std::cout << s1 << " " << s3 << " " << s2;
-    } else if (s2 >= s1 && s1 >= s3) { // s3 s1 s2
-        std::cout << s3 << " " << s1 << " " << s2;
+    int num = 0;
+    std::cout << "Enter the number: ";
+    std::cin >> num;
+    if (num % 2 == 0) {
+        std::cout << "Number" << num << "is even";
+    } else {
+        std::cout << "Number " << num << " is odd";
     }
 }
