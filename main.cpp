580d8fde38ddc911ab37cf7f45757edd1e329b22
Daniel Carvalho <danielgpcarvalho20121212@gmail.com>
2025-12-27 15:44:32 -0300

    Finished exercise 2.7 (page 49)

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 #include "PPPheaders.h"
 
 int main() {
-    int v1, v2, v3;
+    string s1, s2, s3;
     std::cout << "Enter three values: ";
-    std::cin >> v1 >> v2 >> v3;
-    std::cout << v1 << ", " << v2 << " and " << v3 << '\n';
+    std::cin >> s1 >> s2 >> s3;
+    std::cout << s1 << ", " << s2 << " and " << s3 << '\n';
 
-    if (v1 >= v2 && v2 >= v3) {        // v3 v2 v1
-        std::cout << v3 << " " << v2 << " " << v1;
-    } else if (v3 >= v2 && v2 >= v1) { // v1 v2 v3
-        std::cout << v1 << " " << v2 << " " << v3;
-    } else if (v3 >= v1 && v1 >= v2) { // v2 v1 v3
-        std::cout << v2 << " " << v1 << " " << v3;
-    } else if (v1 >= v3 && v3 >= v2) { // v2 v3 v1
-        std::cout << v2 << " " << v3 << " " << v1;
-    } else if (v2 >= v3 && v3 >= v1) { // v1 v3 v2
-        std::cout << v1 << " " << v3 << " " << v2;
-    } else if (v2 >= v1 && v1 >= v3) { // v3 v1 v2
-        std::cout << v3 << " " << v1 << " " << v2;
+    if (s1 >= s2 && s2 >= s3) {        // s3 s2 s1
+        std::cout << s3 << " " << s2 << " " << s1;
+    } else if (s3 >= s2 && s2 >= s1) { // s1 s2 s3
+        std::cout << s1 << " " << s2 << " " << s3;
+    } else if (s3 >= s1 && s1 >= s2) { // s2 s1 s3
+        std::cout << s2 << " " << s1 << " " << s3;
+    } else if (s1 >= s3 && s3 >= s2) { // s2 s3 s1
+        std::cout << s2 << " " << s3 << " " << s1;
+    } else if (s2 >= s3 && s3 >= s1) { // s1 s3 s2
+        std::cout << s1 << " " << s3 << " " << s2;
+    } else if (s2 >= s1 && s1 >= s3) { // s3 s1 s2
+        std::cout << s3 << " " << s1 << " " << s2;
     }
 }
