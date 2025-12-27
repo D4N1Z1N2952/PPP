373783bb7db45e870e06ef8700f531dbad6a1ce2
Daniel Carvalho <danielgpcarvalho20121212@gmail.com>
2025-12-27 08:04:35 -0300

    Finished practice 2.2 (page 47)

--- main.cpp
+++ main.cpp
@@ -4,5 +4,7 @@ int main() {
     cout << "Enter the name of the person you want to send the letter: ";
     string first_name;
     cin >> first_name;
-    cout << "Dear " << first_name << ",\n";
+    cout << "Dear " << first_name << ",\n"
+         << "\tHow are you? I am alright. I miss you so much, I wish\n"
+         << "\twe meet up with each other more often.\n\n";
 }
