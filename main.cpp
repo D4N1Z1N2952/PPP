245c037c5e212088f4784f7e2e73cd14852b7541
Daniel Carvalho <danielgpcarvalho20121212@gmail.com>
2025-12-27 08:22:23 -0300

    Finished practice 2.4 (page 47)

--- main.cpp
+++ main.cpp
@@ -7,8 +7,17 @@ int main() {
     cout << "Dear " << first_name << ",\n"
          << "\tHow are you? I am alright. I miss you so much, I wish\n"
          << "\twe meet up with each other more often.\n\n";
+
     cout << "Enter the other friend name: ";
     string other_friend;
     cin >> other_friend;
-    cout << "Have you seen " << other_friend << " lately?";
+    cout << "\tHave you seen " << other_friend << " lately?\n";
+
+    cout << "Enter your age: ";
+    int age = 0;
+    cin >> age;
+    if (age <= 0 || age >= 110) {
+        error("You're kidding me!");
+    }
+    cout << "\tI heard it was your birthday and now you're " << age << " years old";
 }
