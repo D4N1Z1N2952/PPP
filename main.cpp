7d7df858d125c4a59a80d1b4a15a166e21869ce3
Daniel Carvalho <danielgpcarvalho20121212@gmail.com>
2026-01-30 17:11:41 -0300

    Try this 3.4.2.1 page 66

--- main.cpp
+++ main.cpp
@@ -1,29 +1,10 @@
 #include "PPPheaders.h"
 
-int main() {
-    const double dollar2iene = 159.20;
-    const double dollar2reais = 5.37;
-    const double dollar2pound = 0.74;
-    
-    cout << "Enter an value folowed by its type of money to be converted into dollars: ";
-    double value = 0;
-    char type = ' ';
-    cin >> value >> type;
-    
-    switch (type) {
-      case 'y':
-        value *= dollar2iene;
-        break;
-      case 'p':
-        value *= dollar2pound;
-        break;
-      case 'r':
-        value *= dollar2reais;
-        break;
-      default:
-        cout << "Unknow value" << '\n';
-        break;
-    }
-
-    cout << "THe conveted value is: " << value << type;
+int main () {
+  int i = 0;
+  while (i<=25) {
+    std::cout << char(97 + i) << "\t" << i + 97 << '\n';
+    ++i;
+  }
+  return 0;
 }
